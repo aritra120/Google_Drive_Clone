4aa0b38813692603e1da0ab5f8af71a957d3f7f6
AritraDutta <aritradutta120@gmail.com>
2021-02-05 16:26:09 +0530

    Side Bar added with proper styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/header'
+import SideBar from './components/sidebar'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <SideBar/>
     </div>
   );
 }
